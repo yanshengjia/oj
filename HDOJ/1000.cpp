72b839125ef8d3d8e6d921d1699c0471ffb4ca11
muscle <yanshengjia1995@163.com>
2015-09-27 09:25:18 +0800

    Update

---
+++ HDOJ/1000.cpp
@@ -0,0 +1,6 @@
+//HDOJ #1000
+//A+B
+main(a,b)
+{
+    for(;~scanf("%d%d",&a,&b);printf("%d\n",a+b));
+}
--- HDOJ/1001.cpp
+++ HDOJ/1001.cpp
@@ -1,14 +1,37 @@
-//HDOJ
+//
+//
+
 
 #include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <stdio.h>
+
 using namespace std;
+
+int sum(int x)
+{
+	long long int a = x;
+	int answer = (int)((1+a)*a/2);
+	return answer;
+}
+
+
 
 int main()
 {
-	int n;
-	cout << "请输入一个数：" << endl;
-	cin >> n;
-	cout << (1 + n)*n*0.5;
-	system("pause");
-	return 0;
+    int a;
+
+    while(scanf("%d", &a)!=EOF)
+    {
+    	printf("%d\n\n", sum(a));
+    }
+    
+    return 0;
 }
