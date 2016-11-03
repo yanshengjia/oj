71d56e4ff20f19064800d5c1c7507384fe9575ad
lurenjia <yanshengjia1995@163.com>
2016-11-03 15:30:59 +0800

    Update

---
+++ LeetCode/292.cpp
@@ -0,0 +1,11 @@
+// created by sjyan @2016-11-03
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4 == 0)
+            return false;
+        else 
+            return true;
+    }
+};
