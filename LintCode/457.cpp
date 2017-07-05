89e277928aef8fe9d9bc2640b7cc35836eb3c3dd
sjyan <sjyan@seu.edu.cn>
2017-07-06 03:04:30 +0800

    add some answers

--- LintCode/457.cpp
+++ LintCode/457.cpp
@@ -1,4 +1,52 @@
 // Author: Shengjia Yan
 // Date: 2017年7月1日
 // Email: sjyan@seu.edu.cn
-// Time Complexity: O(n)    Space Complexity: O(1)
+// Time Complexity: O(logn)    Space Complexity: O(1)
+
+
+class Solution {
+public:
+    /**
+     * @param A an integer array sorted in ascending order
+     * @param target an integer
+     * @return an integer
+     */
+    int findPosition(vector<int>& A, int target) {
+        // Write your code here
+        int size = A.size();
+        int mid = size / 2;
+        int index = mid;
+        bool flag = false;
+        
+        if (size == 0)  return -1;
+        
+        if (A[mid] == target) {
+            flag = true;
+            return index;
+        }
+        else if (A[mid] > target) {
+            // <-
+            index--;
+            while (index >= 0) {
+                if (A[index] == target) {
+                    flag = true;
+                    return index;
+                }
+                index--;
+            } 
+        }
+        else {
+            // ->
+            index++;
+            while (index < size) {
+                if (A[index] == target) {
+                    flag = true;
+                    return index;
+                }
+                index++;
+            } 
+        }
+        
+        if (!flag)  return -1;
+    }
+};
